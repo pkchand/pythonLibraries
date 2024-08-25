0d46341324deee677992d6a75c2dabdb6e5b0d81
Pralay Chand <pralay1991chand@gmail.com>
2024-08-26 00:07:18 +0530

    new comit on string

--- strings.py
+++ strings.py
@@ -28,12 +28,34 @@ def reverse_string_without_space_location(str):
 
 def string_subset(str1, str2):
     """takes a string and checks if that string contains a separately given string"""
-    
 
 
+def swap_case(s):
+    str=""
+    for i in s:
+        if i.isalpha():
+            if i.islower():
+                i=i.upper()
+            else:
+                i=i.lower()
+        str+=i
+            
+    print(str)
 
 
-reverse_words_in_sentense("sly fox jumps over the fence")
+def split_and_join(line):
+    # write your code here
+    spilted=line.split(" ")
+    result=""
+    for i in spilted:
+        result=result+i+"-"
+    print(result.strip("-"))
+
+split_and_join("This is my country")
+
+#swap_case("ok Bye")
+
+#reverse_words_in_sentense("sly fox jumps over the fence")
 #reverse_string("determined")
 #count_vowels("jump over the building")
 
