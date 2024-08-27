379d2cd1313c09b1d4fd0c93d7f9cca316915016
Pralay Chand <pralay1991chand@gmail.com>
2024-08-27 22:32:25 +0530

    question on lists

---
+++ list.py
@@ -0,0 +1,9 @@
+import random
+
+friends=["Papri","Pralay", "Sanath", "Prama", "Siva"]
+selected_person=random.choice(friends)
+print(selected_person)
+
+randon_index=random.randint(0, len(friends)-1)
+print(friends[randon_index])
+
